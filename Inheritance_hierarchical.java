545351df8d67377e40dad007f5a6d275514c1d99
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-14 17:04:34 +0530

    Add files via upload

---
+++ Inheritance_hierarchical.java
@@ -0,0 +1,42 @@
+package corejava;
+
+class P
+{
+	void input()
+	{
+		System.out.println("Enter your Name");
+	}
+}
+
+class Q extends P
+{
+	void Show()
+	{
+		System.out.println("My Name Is Ajju");
+	}
+}
+
+class R extends P
+{
+	void Disp()
+	{
+		System.out.println("My Name Is Appu");
+	}
+}
+
+
+public class Inheritance_hierarchical 
+{
+	public static void main(String[] args) 
+	{
+		Q r=new Q();
+		R r1=new R();
+		
+		r.input();
+		r.Show();
+		r1.input();
+		r1.Disp();
+
+	}
+
+}
