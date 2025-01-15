f190b1734aa0e3b9510214ee66c50e20b909afb7
Jayant <154452257+jay11nt@users.noreply.github.com>
2025-01-15 16:11:31 +0530

    Add files via upload

---
+++ Inheritance_multiple.java
@@ -0,0 +1,33 @@
+package corejava;
+
+interface G
+{
+	void show();     //abstract method
+}
+
+interface H
+{
+	void disp();
+}
+
+
+public class Inheritance_multiple implements G,H
+{
+	public void show()
+	{
+		System.out.println("Interface H");
+	}
+	
+	public void disp()
+	{
+		System.out.println("Interface G");
+	}
+
+	public static void main(String[] args) 
+	{
+		Inheritance_multiple m=new Inheritance_multiple();
+		m.show();
+		m.disp();
+    }
+
+}
